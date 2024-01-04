2d6c80ebda1562d9c0dd22150a8082ff2fe10911
linlinger <32541118+linlinger@users.noreply.github.com>
2024-01-04 16:22:22 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,4 @@ rm -rf hardware/xiaomi/aidl/power-libperfmgr
 
 git clone --depth=1 https://github.com/GinkgoHub/android_vendor_xiaomi_ginkgo.git -b udc vendor/xiaomi/ginkgo
 git clone --depth=1 https://github.com/GinkgoHub/android_kernel_xiaomi_ginkgo.git -b udc kernel/xiaomi/ginkgo
+git clone --depth=1 https://github.com/LineageOS/android_device_qcom_sepolicy_vndr.git -b lineage-21.0-legacy-um device/qcom/sepolicy_vndr-legacy-um
