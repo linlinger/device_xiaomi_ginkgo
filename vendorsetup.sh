590b62c86b5e9bf6e76c4948b3c3fd39b09c3c88
linlinger <32541118+linlinger@users.noreply.github.com>
2024-02-24 21:31:08 +0800

    Update vendorsetup.sh
    add kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-git clone https://github.com/tejas101k/vendor_xiaomi_ginkgo-udc --depth=1 vendor/xiaomi/ginkgo
+git clone --depth=1 https://github.com/tejas101k/vendor_xiaomi_ginkgo-udc vendor/xiaomi/ginkgo
+git clone --depth=1 https://github.com/crdroidandroid/android_kernel_xiaomi_ginkgo.git kernel/xiaomi/ginkgo
 
